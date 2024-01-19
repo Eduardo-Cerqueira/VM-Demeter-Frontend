5ec2ce6983ab8093ce0e131101094354ee684770
Val <pro.mvaldo@gmail.com>
2024-01-19 09:24:02 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue';
+
+
+const data = ref([])
+
+const addData = () => {
+  data.value.push("Test")
+}
+</script>
+
+<template>
+  <main>
+    <button @click="addData">GENERATE</button>
+    <div class="data-wrapper">
+      <div class="data-card" v-for="el, index in data" :key="index">
+      {{ el }} 
+    </div>
+    </div>
+    
+  </main>
+</template>
+
+<style scoped>
+
+</style>
